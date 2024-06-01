ff6ab07cb7dc9cfb0b95734f275d1e5797e46be9
Bartlett-S <bartsama87@gmail.com>
2024-06-01 10:47:38 -0500

    Finished first learning section

---
+++ Python coding with turtle/circles.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+color("red")
+circle(200)
+color("green")
+circle(100)
+color("purple")
+circle(50)
---
+++ Python coding with turtle/colors.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+bgcolor("black")
+
+color("red")
+
+begin_fill()
+
+
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+end_fill()
+
+done()
